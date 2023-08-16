970430aeb015333dba42a6845286c53df5def5f7
sergiolara1992 <sergiolaradiaz3@gmail.com>
2023-08-15 22:14:22 -0500

    Components

--- Trade_View/src/components/Header.vue
+++ Trade_View/src/components/Header.vue
@@ -1,7 +1,30 @@
 <template>
+     <header class="header">
+          <div class="header_barra contenedor">
+              <div class="header__logo">
+                  <img src="../public/Logo.svg" alt="" srcset="">
+              </div>
+
+              <nav class="nav-header">
+                  <a class="links" href="#">FOREX</a>
+                  <a class="links" href="#">STOCKS</a>
+                  <a class="links" href="#">FUTURES</a>
+                  <a class="links" href="#">TradeGATEHub</a>
+              </nav>
+
+              <div class="intro">
+                <label class="log" for="">Log in</label>
+                <select name="log in" id="">Log in</select>
+                <a class="contact" href="">Contact us</a>
+                <label class="contact" for="">English  </label>
+                <select name="" id=""></select>
+              </div>
+          </div>
+      </header>
 
 </template>
 
 <script setup>
+
 
 </script>
